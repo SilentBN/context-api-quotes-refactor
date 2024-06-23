15e9114538c15b66748cc47e9cb362524fd5d174
SilentBN <61912362+SilentBN@users.noreply.github.com>
2024-06-23 16:04:31 -0400

    chore: Add reducer function to quotesContext.js

--- frontend/context/quotesContext.js
+++ frontend/context/quotesContext.js
@@ -34,3 +34,34 @@ const initialState = {
     },
   ],
 };
+
+// Reducer function
+const reducer = (state, action) => {
+  switch (action.type) {
+    case CREATE_QUOTE:
+      return { ...state, quotes: [...state.quotes, action.payload] };
+    case DELETE_QUOTE:
+      return {
+        ...state,
+        quotes: state.quotes.filter((qt) => qt.id !== action.payload),
+      };
+    case EDIT_QUOTE_AUTHENTICITY:
+      return {
+        ...state,
+        quotes: state.quotes.map((qt) => {
+          if (qt.id != action.payload) return qt;
+          return { ...qt, apocryphal: !qt.apocryphal };
+        }),
+      };
+    case SET_HIGHLIGHTED_QUOTE:
+      return {
+        ...state,
+        highlightedQuote:
+          state.highlightedQuote !== action.payload ? action.payload : null,
+      };
+    case TOGGLE_VISIBILITY:
+      return { ...state, displayAllQuotes: !state.displayAllQuotes };
+    default:
+      return state;
+  }
+};
