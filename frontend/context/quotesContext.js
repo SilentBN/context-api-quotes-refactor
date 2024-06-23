2d66e31860dac20125f959ff5f7699dc60799fdf
SilentBN <61912362+SilentBN@users.noreply.github.com>
2024-06-23 16:03:57 -0400

    initialState and Action Types

--- frontend/context/quotesContext.js
+++ frontend/context/quotesContext.js
@@ -1,1 +1,36 @@
 // Create your context here
+import React, { createContext, useReducer } from "react";
+
+// Action Types
+const CREATE_QUOTE = "CREATE_QUOTE";
+const DELETE_QUOTE = "DELETE_QUOTE";
+const EDIT_QUOTE_AUTHENTICITY = "EDIT_QUOTE_AUTHENTICITY";
+const SET_HIGHLIGHTED_QUOTE = "SET_HIGHLIGHTED_QUOTE";
+const TOGGLE_VISIBILITY = "TOGGLE_VISIBILITY";
+
+let id = 1;
+const getNextId = () => id++;
+const initialState = {
+  displayAllQuotes: true,
+  highlightedQuote: null,
+  quotes: [
+    {
+      id: getNextId(),
+      quoteText: "Don't cry because it's over, smile because it happened.",
+      authorName: "Dr. Seuss",
+      apocryphal: false,
+    },
+    {
+      id: getNextId(),
+      quoteText: "So many books, so little time.",
+      authorName: "Frank Zappa",
+      apocryphal: false,
+    },
+    {
+      id: getNextId(),
+      quoteText: "Be yourself; everyone else is already taken.",
+      authorName: "Oscar Wilde",
+      apocryphal: false,
+    },
+  ],
+};
